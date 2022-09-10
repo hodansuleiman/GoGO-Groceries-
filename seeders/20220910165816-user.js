0a4958f441a4f8bd1bf2b1d97f5f5a174f6f2921
Hodan Suleiman <suleimanhodan9@gmail.com>
2022-09-10 13:15:52 -0400

    add default user

---
+++ seeders/20220910165816-user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+     await queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Andrea',
+        lastName: 'Anderson',
+        email: 'andrea@someemail.com',
+        password: 'somePlainTextPasswordForNow',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
